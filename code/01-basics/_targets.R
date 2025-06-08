9062b05d6fa2d10e4941f44371fc35ad3ad59f6e
Rahul S <rahul.sangole@gmail.com>
2025-06-08 08:15:14 -0700

    some code

---
+++ code/01-basics/_targets.R
@@ -0,0 +1,97 @@
+setwd(here::here("code/01-basics"))
+
+library(targets)
+library(dplyr)
+library(janitor)
+library(plotly)
+library(NHANES)
+
+list(
+  tar_target(
+    tbl_NHANES_subset,
+    NHANES |>
+      select(
+        ID,
+        SurveyYr,
+        Gender,
+        Age,
+        Education,
+        MaritalStatus,
+        HHIncome,
+        HomeOwn,
+        Work,
+        Diabetes,
+        BMI,
+        TotChol,
+        BPSysAve,
+        BPDiaAve
+      )
+  ),
+  
+  
+  # Cleanup
+  tar_target(
+    tbl_clean,
+    tbl_NHANES_subset |>
+      janitor::clean_names() |>
+      tidyr::drop_na()
+  ),
+  
+  
+  
+  # Summaries
+  tar_target(tbl_means, tbl_clean |>
+               summarise(
+                 across(where(is.numeric), mean), .by = c("gender", "diabetes")
+               )),
+  
+  tar_target(tbl_counts, tbl_clean |>
+               summarise(
+                 n = n(),
+                 .by = c("gender", "education", "marital_status")
+               )),
+  
+  
+  
+  # Graphs
+  tar_target(
+    plot_scatter,
+    plot_ly(
+      tbl_clean,
+      x = ~ bp_dia_ave,
+      y = ~ bp_sys_ave,
+      color = ~ diabetes,
+      alpha = 0.4
+    ) |>
+      add_markers()
+  ),
+  
+  tar_target(
+    plot_bar,
+    
+    plot_ly(
+      tbl_means,
+      x = ~ bmi,
+      y = ~ diabetes,
+      color = ~ gender
+    ) |>
+      add_bars()
+  ),
+  
+  
+  # Model
+  tar_target(
+    mod_lm,
+    {
+      tbl_2 <- tbl_clean |>
+        filter(bp_dia_ave > 0, bp_sys_ave > 0)
+      
+      lm(bp_dia_ave ~ bp_sys_ave + gender + age, tbl_2)
+    }
+  ),
+  
+  tar_target(
+    mod_lm_summary,
+    summary(mod_lm)
+  )
+)
---
+++ code/01-basics/a_script.R
@@ -0,0 +1,74 @@
+setwd(here::here("code/01-basics"))
+
+library(dplyr)
+library(janitor)
+library(plotly)
+library(NHANES)
+
+glimpse(NHANES)
+
+# Subset
+tbl_NHANES_subset <- NHANES |>
+  select(
+    ID,
+    SurveyYr,
+    Gender,
+    Age,
+    Education,
+    MaritalStatus,
+    HHIncome,
+    HomeOwn,
+    Work,
+    Diabetes,
+    BMI,
+    TotChol,
+    BPSysAve,
+    BPDiaAve
+  )
+tbl_NHANES_subset
+
+# Cleanup
+tbl_clean <- tbl_NHANES_subset |>
+  janitor::clean_names() |>
+  tidyr::drop_na()
+tbl_clean
+
+# Summaries
+tbl_means <- tbl_clean |>
+  summarise(across(where(is.numeric), mean), .by = c("gender", "diabetes"))
+tbl_means
+
+tbl_counts <- tbl_clean |>
+  summarise(
+    n = n(),
+    .by = c("gender", "education", "marital_status")
+  )
+tbl_counts
+
+
+# Graphs
+plot_ly(
+  tbl_clean,
+  x = ~bp_dia_ave,
+  y = ~bp_sys_ave,
+  color = ~diabetes,
+  alpha = 0.4
+) |> 
+  add_markers()
+
+plot_ly(
+  tbl_means,
+  x = ~bmi,
+  y = ~diabetes,
+  color = ~gender
+) |> 
+  add_bars()
+
+
+# Model
+tbl_2 <- tbl_clean |> 
+  filter(bp_dia_ave>0, bp_sys_ave>0)
+
+mod_lm <- lm(bp_dia_ave~bp_sys_ave+gender+age, tbl_2)
+summary(mod_lm)
+plot(mod_lm)
