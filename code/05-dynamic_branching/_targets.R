645227c425650df774e6ca154a2ced3518f7dbde
Rahul S <rahul.sangole@gmail.com>
2025-06-22 05:53:16 -0700

    Cleans up repo. Adds solutions.

--- code/05-dynamic_branching/_targets.R
+++ code/05-dynamic_branching/_targets.R
@@ -115,24 +115,5 @@ list(
 
 
   # Solution ----
-  tar_target(
-    vec_age_decade,
-    unique(tbl_NHANES$age_decade)
-  ),
-  tar_target(
-    tbl_statistics,
-    {
-      tbl_NHANES |>
-        dplyr::filter(
-          age_decade == vec_age_decade
-        ) |>
-        dplyr::summarise(
-          mean_bmi = mean(bmi, na.rm = TRUE),
-          sd_bmi = sd(bmi, na.rm = TRUE),
-          .by = "age_decade"
-        )
-    },
-    pattern = map(vec_age_decade),
-    iteration = "list"
-  )
+
 )
